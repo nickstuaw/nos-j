47fb86b5af8ee24c9b08497e13a2b018ec610aac
nsgw <dev@nsgw.xyz>
2022-01-07 10:30:02 +0000

    Update Reference.java

--- src/xyz/nsgw/tools/nscript/Reference.java
+++ src/xyz/nsgw/tools/nscript/Reference.java
@@ -1,28 +1,30 @@
 package xyz.nsgw.tools.nscript;
-
+// An object used to match an identifier with its value.
+// Used in nscript when setting variables and calling methods.
 public class Reference {
-
+    // Initialize the reference's local identifier
     private final String identifier;
-
+    // and value
     private String value;
-
+    // Constructor - take the identifier if the value hasn't already been provided.
     public Reference(String identifier) {
+        // Strip / trim the identifier of any trailing whitespace.
         this.identifier = identifier.stripTrailing();
     }
-
+    // Constructor - take and set the identifier and the value.
     public Reference(String identifier, String value) {
         this.identifier = identifier.stripLeading().stripTrailing();
         this.value = value.stripLeading().stripTrailing();
     }
-
+    // Get the value.
     public String getValue() {
         return value;
     }
-
+    // Get the identifier.
     public String getIdentifier() {
         return identifier;
     }
-
+    // Strip / trim and set a new value.
     public void setValue(String value) {
         this.value = value.stripLeading().stripTrailing();
     }
